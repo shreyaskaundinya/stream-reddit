9f75de19b1655a201293513ebf2e25d3290eeb8c
Smriti <77257191+smriti0302@users.noreply.github.com>
2023-04-23 10:06:29 +0530

    Update spark_consumer.py

--- spark_consumer.py
+++ spark_consumer.py
@@ -2,9 +2,10 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.streaming import StreamingContext
 from kafka import KafkaConsumer
-
+kafka_broker = 'localhost:9092'
+kafka_topic = 'reddit_dbt'
 #spark = SparkSession.builder.appName("DataConsumer").getOrCreate()
-kafka_consumer = KafkaConsumer("reddit_data", bootstrap_servers='localhost:9092')
+kafka_consumer = KafkaConsumer(kafka_topic, bootstrap_servers=kafka_broker)
 
 for message in kafka_consumer:
     reddit_data = message.value.decode('utf-8')
