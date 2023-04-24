7b834752dc244262e819c2436981b003f3f365e6
Smriti <77257191+smriti0302@users.noreply.github.com>
2023-04-24 21:23:10 +0530

    Create consumer.py

---
+++ consumer/consumer.py
@@ -0,0 +1,12 @@
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+analyzer = SentimentIntensityAnalyzer()
+def analyzer_function(post):
+	sentiment = analyzer.polarity_scores(post)
+	if(sentiment["compound"]>0.05):
+		return "good"
+	elif(sentiment["compound"]<0.05):
+		return "bad"
+	else:
+		return "neutral"
+
